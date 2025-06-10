0cc840524a44533471bbaa1e9e2bb4f374e4e96c
Leorevoir <leo.quinzler@epitech.eu>
2025-06-10 12:59:04 +0200

    fix: compilation issues

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,31 @@
 ** Main.cpp
 */
 
+// clang-format off
+#if defined(__clang__)
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Wundef"
+#elif defined(__GNUC__)
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wundef"
+#endif
+#include <raylib.h>
+#if defined(__clang__)
+    #pragma clang diagnostic pop
+#elif defined(__GNUC__)
+    #pragma GCC diagnostic pop
+#endif
+// clang-format on
+
 int main(void)
 {
+    InitWindow(1920, 1080, "Zappy");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Welcome to Zappierdolekurwa!", 190, 280, 20, PURPLE);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
