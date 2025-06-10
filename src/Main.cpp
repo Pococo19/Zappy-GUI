6c8a0ece080c848fec515fbe0a23c1349bed7abc
Leorevoir <leo.quinzler@epitech.eu>
2025-06-10 16:39:16 +0200

    add: main render loop

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,31 +5,43 @@
 ** Main.cpp
 */
 
-// clang-format off
-#if defined(__clang__)
-    #pragma clang diagnostic push
-    #pragma clang diagnostic ignored "-Wundef"
-#elif defined(__GNUC__)
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wundef"
-#endif
-#include <raylib.h>
-#if defined(__clang__)
-    #pragma clang diagnostic pop
-#elif defined(__GNUC__)
-    #pragma GCC diagnostic pop
-#endif
-// clang-format on
+#include <memory>
+
+#include "ZapGUI/Drawable/Model.hpp"
+#include <ZapGUI/Raylib.hpp>
 
 int main(void)
 {
     InitWindow(1920, 1080, "Zappy");
+
+    std::unique_ptr<zap::abstract::Drawable> model = std::make_unique<zap::ZapModel>("assets/models/CommonTree_1.obj", "assets/textures/");
+
+    Camera camera;
+    camera.position = {4.0f, 4.0f, 4.0f};
+    camera.target = {0.0f, 0.0f, 0.0f};
+    camera.up = {0.0f, 1.0f, 0.0f};
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
+        UpdateCamera(&camera, CAMERA_FREE);
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Welcome to Zappierdolekurwa!", 190, 280, 20, PURPLE);
+
+        BeginMode3D(camera);
+        model->draw();
+        DrawGrid(10, 1.0f);
+        EndMode3D();
+
+        DrawText("ESC to leave", 10, 10, 20, DARKGRAY);
         EndDrawing();
     }
+
+    model.reset();
     CloseWindow();
+
     return 0;
 }
