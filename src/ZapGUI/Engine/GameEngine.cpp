acff4e2fe01e1d1ebac9641ab3f9cb5eb4b65779
Leorevoir <leo.quinzler@epitech.eu>
2025-06-11 12:01:11 +0200

    add: graphical engine

---
+++ src/ZapGUI/Engine/GameEngine.cpp
@@ -0,0 +1,65 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** GameEngine.cpp
+*/
+
+#include <ZapGUI/Engine/GameEngine.hpp>
+#include <ZapGUI/Logger.hpp>
+
+/**
+* public
+*/
+
+void zap::abstract::GameEngine::startup()
+{
+    logger::task_start("initializing game engine...");
+    this->init();
+    logger::task_done("game engine initialized");
+}
+
+void zap::abstract::GameEngine::render()
+{
+    for (const auto &scene : _scenes) {
+        scene.second->render();
+    }
+}
+
+/**
+* protected
+*/
+
+void zap::abstract::GameEngine::addScene(const std::string &name, std::unique_ptr<render::Scene> scene)
+{
+    if (_scenes.contains(name)) {
+        logger::debug("scene ", name, " already exists");
+        return;
+    }
+    _scenes.emplace(name, std::move(scene));
+}
+
+void zap::abstract::GameEngine::addToScene(const std::string &name, std::unique_ptr<abstract::Drawable> object)
+{
+    if (!_scenes.contains(name)) {
+        logger::debug("scene ", name, "does not exist, creating it");
+        this->addScene(name, std::make_unique<render::Scene>());
+    }
+    _scenes.at(name)->add(std::move(object));
+}
+
+void zap::abstract::GameEngine::addCamera(const std::string &name, std::unique_ptr<ZapCamera> camera)
+{
+    if (!_scenes.contains(name)) {
+        logger::debug("scene ", name, "does not exist, creating it");
+        this->addScene(name, std::make_unique<render::Scene>());
+    }
+    _scenes.at(name)->add(std::move(camera));
+}
+
+void zap::abstract::GameEngine::shutdown()
+{
+    logger::task_start("shutting down game engine...");
+    _scenes.clear();
+    logger::task_done("game engine shut down");
+}
---
+++ src/ZapGUI/Engine/GameEngine.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy
+** File description:
+** GameEngine.hpp
+*/
+
+#pragma once
+
+#include <ZapGUI/Engine/RenderEngine.hpp>
+#include <ZapGUI/Render/Scene.hpp>
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+namespace zap::abstract {
+
+class GameEngine : public abstract::RenderEngine
+{
+    public:
+        virtual ~GameEngine() = default;
+
+        void startup() override;
+        void render() override;
+        void shutdown() override;
+
+    protected:
+        void addScene(const std::string &name, std::unique_ptr<render::Scene> scene);
+        void addToScene(const std::string &name, std::unique_ptr<abstract::Drawable> object);
+        void addCamera(const std::string &name, std::unique_ptr<ZapCamera> camera);
+
+    private:
+        std::unordered_map<std::string, std::unique_ptr<render::Scene>> _scenes;
+};
+
+}// namespace zap::abstract
