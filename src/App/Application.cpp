90d9b78b4f6ecd1a4fbf6693740cbf0957cdca26
Leorevoir <leo.quinzler@epitech.eu>
2025-06-11 12:00:50 +0200

    add: Zappy Application

---
+++ src/App/Application.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy-GUI
+** File description:
+** Application.cpp
+*/
+
+#include <ZapGUI/Drawable/Model.hpp>
+
+#include <App/Application.hpp>
+
+/**
+* public
+*/
+
+static std::unique_ptr<zap::abstract::Drawable> _create_model(const std::string &obj_path)
+{
+    return std::make_unique<zap::ZapModel>(obj_path, "assets/textures/");
+}
+
+void zappy::Application::init()
+{
+    addScene("main", std::make_unique<zap::render::Scene>());
+    addToScene("main", _create_model("assets/models/CommonTree_1.obj"));
+    addCamera("main", std::make_unique<zap::ZapCamera>());
+}
