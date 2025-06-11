13df01eae2216939e23d141c7bd440037fc256e5
Leorevoir <leo.quinzler@epitech.eu>
2025-06-11 12:00:16 +0200

    feat: better main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,38 +5,10 @@
 ** Main.cpp
 */
 
-#include "ZapGUI/Render/Camera.hpp"
-#include <memory>
-
-#include <ZapGUI/Drawable/Model.hpp>
-#include <ZapGUI/Raylib.hpp>
+#include <App/Application.hpp>
+#include <ZapGUI/Context.hpp>
 
 int main(void)
 {
-    InitWindow(1920, 1080, "Zappy");
-
-    std::unique_ptr<zap::abstract::Drawable> model = std::make_unique<zap::ZapModel>("assets/models/CommonTree_1.obj", "assets/textures/");
-    zap::ZapCamera camera;
-
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose()) {
-        camera.update();
-
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        BeginMode3D(camera.get());
-        model->draw();
-        DrawGrid(10, 1.0f);
-        EndMode3D();
-
-        DrawText("ESC to leave", 10, 10, 20, DARKGRAY);
-        EndDrawing();
-    }
-
-    model.reset();
-    CloseWindow();
-
-    return 0;
+    return zap::context::run(std::make_unique<zappy::Application>(), {1920, 1080}, "Zappy", 120);
 }
