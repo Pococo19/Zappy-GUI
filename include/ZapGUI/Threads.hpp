a8dc16450d8bac514925697ce155848f733e8015
Leorevoir <leo.quinzler@epitech.eu>
2025-06-16 15:36:42 +0200

    add: thread-safe function

---
+++ include/ZapGUI/Threads.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy-GUI
+** File description:
+** Threads.hpp
+*/
+
+#pragma once
+
+#ifdef async
+    #pragma message("async macro already defined, undefining it")
+    #undef async
+#endif
+
+#define async(func, ...)                                                                                                                                                 \
+    void func##_impl(__VA_ARGS__);                                                                                                                                       \
+    template<typename... Args>                                                                                                                                           \
+    static inline void func(Args &&...args)                                                                                                                              \
+    {                                                                                                                                                                    \
+        std::thread([=]() { func##_impl(args...); }).detach();                                                                                                           \
+    }                                                                                                                                                                    \
+    void func##_impl(__VA_ARGS__)
