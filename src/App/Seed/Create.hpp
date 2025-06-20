0fad1d5465baf416b5da5398c40e58a3073cf0ae
Leorevoir <leo.quinzler@epitech.eu>
2025-06-20 20:18:59 +0200

    rm: copy-paste alread-defined functions

--- src/App/Seed/Create.hpp
+++ src/App/Seed/Create.hpp
@@ -7,7 +7,18 @@
 
 #pragma once
 
+#if !defined(MIN_DISTANCE)
+    #define MIN_DISTANCE 2.0f
+#endif
+#if !defined(MAX_ATTEMPTS)
+    #define MAX_ATTEMPTS 100
+#endif
+#if !defined(COUNT_RANDOM_RANGE)
+    #define COUNT_RANDOM_RANGE (2.5f, 3.0f)
+#endif
+
 #define ZAP_USE_RAYLIB_MATH
+#include <App/Maths/Maths.hpp>
 #include <App/Stars/BasePlanet.hpp>
 #include <ZapGUI/Drawable/Model.hpp>
 #include <ZapGUI/Filename.hpp>
@@ -35,6 +46,26 @@ static inline std::shared_ptr<zap::ZapModel> model(const std::string &obj_path, 
 
     model->setPosition(position);
     return model;
+}
+
+void seed(std::shared_ptr<zap::render::Scene> &scene, const std::vector<std::string> &obj_paths, const f32 radius, const u32 count, const Vector3 &scale);
+
+[[nodiscard]] static inline u32 get_count(const f32 radius)
+{
+    const f32 spacing_offset = maths::random<f32> COUNT_RANDOM_RANGE;
+
+    /** @brief A = 4πr² */
+    const f32 surface = 4.f * PI * radius * radius;
+    const f32 count = spacing_offset * spacing_offset;
+
+    return static_cast<u32>(surface / count);
+}
+
+[[nodiscard]] static inline constexpr Vector3 get_scale(const f32 radius, const u32 width, const u32 height)
+{
+    const f32 scale_factor = std::max(static_cast<f32>(width), static_cast<f32>(height)) / (2.0f * radius) / 10;
+
+    return {scale_factor, scale_factor, scale_factor};
 }
 
 void rocks(const protocol::GUI_Map &map, std::shared_ptr<zap::render::Scene> &out_scene, const f32 radius);
--- src/App/Seed/Player.cpp
+++ src/App/Seed/Player.cpp
@@ -5,73 +5,16 @@
 ** Player.cpp
 */
 
-#include "App/Maths/Maths.hpp"
-#include "App/Seed/Create.hpp"
-#include "Create.hpp"
-
-#define MIN_DISTANCE 2.0f
-#define MAX_ATTEMPTS 100
-#define COUNT_RANDOM_RANGE (2.5f, 3.0f)
+#include <App/Seed/Create.hpp>
 
 static const std::vector<std::string> _player_models = {
     zap::Filename::getPath("assets/models/Player2.obj"),
 };
 
-static void _seed(std::shared_ptr<zap::render::Scene> &scene, const std::vector<std::string> &obj_paths, const f32 radius, const u32 count, const Vector3 &scale)
-{
-    constexpr Vector3 center = {0, 0, 0};
-
-    for (u32 i = 0; i < count; ++i) {
-        Vector3 pos;
-
-        for (u32 attempt = 0; attempt < MAX_ATTEMPTS; ++attempt) {
-            const f32 theta = static_cast<f32>(M_PI * (std::rand() / static_cast<f64>(RAND_MAX)));    //<< 0 to π
-            const f32 phi = static_cast<f32>(2.0 * M_PI * (std::rand() / static_cast<f64>(RAND_MAX)));//<< 0 to 2π
-
-            pos = {radius * sinf(theta) * cosf(phi), radius * cosf(theta), radius * sinf(theta) * sinf(phi)};
-            pos = Vector3Add(center, pos);
-
-            if (!zappy::create::is_too_close(pos, MIN_DISTANCE)) {
-                break;
-            }
-        }
-
-        zappy::create::__positions.push_back(pos);
-
-        const auto obj = zappy::create::model(obj_paths[(zappy::maths::random<u32>(0, static_cast<u32>(obj_paths.size() - 1)))], pos);
-
-        f32 angle;
-        const Vector3 rotation = zappy::maths::rotation(pos, &angle);
-
-        obj->setRotationAxis(rotation, angle);
-        obj->setScale(scale);
-
-        scene->add(std::move(obj));
-    }
-}
-
-[[nodiscard]] static u32 _get_count(const f32 radius)
-{
-    const f32 spacing_offset = zappy::maths::random<f32> COUNT_RANDOM_RANGE;
-
-    /** @brief A = 4πr² */
-    const f32 surface = 4.f * PI * radius * radius;
-    const f32 count = spacing_offset * spacing_offset;
-
-    return static_cast<u32>(surface / count);
-}
-
-[[nodiscard]] static constexpr Vector3 _get_scale(const f32 radius, const u32 width, const u32 height)
-{
-    const f32 scale_factor = std::max(static_cast<f32>(width), static_cast<f32>(height)) / (2.0f * radius) / 10;
-
-    return {scale_factor, scale_factor, scale_factor};
-}
-
 void zappy::create::player(std::shared_ptr<zap::render::Scene> &scene, const f32 radius, const u32 width, const u32 height)
 {
-    const u32 count = _get_count(radius);
-    const Vector3 scale = _get_scale(radius, width, height);
+    const u32 count = get_count(radius);
+    const Vector3 scale = get_scale(radius, width, height);
 
-    _seed(scene, _player_models, radius, count, scale);
+    seed(scene, _player_models, radius, count, scale);
 }
