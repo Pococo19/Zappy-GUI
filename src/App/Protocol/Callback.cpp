4865c78562c93164d191392b2f3c1571cc247880
Leorevoir <leo.quinzler@epitech.eu>
2025-06-25 13:52:06 +0200

    deklaratywny protokół serwer-klient kurwa^_^

---
+++ src/App/Protocol/Callback.cpp
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2025
+** Zappy-GUI
+** File description:
+** Callback.cpp
+*/
+
+#include <App/Protocol/Callback.hpp>
+#include <ZapGUI/Logger.hpp>
+
+/**
+* public
+*/
+
+zappy::protocol::Callback &zappy::protocol::Callback::getInstance()
+{
+    static Callback instance;
+
+    return instance;
+}
+
+void zappy::protocol::Callback::add(const std::string &cmd, const std::function<void(const std::string &)> &callback)
+{
+    if (contains(cmd)) {
+        zap::logger::debug("WARN! command ", cmd, " already exists, overwriting it");
+    }
+    _commands[cmd] = callback;
+}
+
+bool zappy::protocol::Callback::contains(const std::string &cmd) const
+{
+    return _commands.contains(cmd);
+}
+
+void zappy::protocol::Callback::call(const std::string &cmd, const std::string &data) const
+{
+    if (!contains(cmd)) {
+        throw zap::exception::Error("Callback", "No callback registered for command: ", cmd);
+    }
+    _commands.at(cmd)(data);
+}
+
+void zappy::protocol::Callback::clear()
+{
+    _commands.clear();
+}
--- src/App/Stars/Slavia.cpp
+++ src/App/Stars/Slavia.cpp
@@ -5,8 +5,16 @@
 ** Slavia.cpp
 */
 
-#include "App/Maths/Maths.hpp"
+#include <App/Maths/Maths.hpp>
 #include <App/Stars/Slavia.hpp>
+
+#define SLAVIA_COLOR {0.8f, 0.3f, 0.1f}
+#define SLAVIA_RADIUS_MULTIPLIER 0.75f
+#define SLAVIA_INCLINATION_DEG 23.5f
+
+#define SLAVIA_SPEED 0.5f
+#define SLAVIA_OFFSET 4.5f
+#define SLAVIA_ROTATION_SPEED 0.6f
 
 /**
 * public
@@ -15,18 +23,18 @@
 zappy::Slavia::Slavia(std::shared_ptr<zap::ZapCamera> camera, const f32 zappy_radius) : BasePlanet(camera)
 {
     _zappy_radius = zappy_radius;
-    _radius = zappy_radius * 0.75f;
+    _radius = zappy_radius * SLAVIA_RADIUS_MULTIPLIER;
     _init(zappy_radius);
     _model->setShader(_shader);
-    setColor({0.8f, 0.3f, 0.1f});
+    setColor(SLAVIA_COLOR);
 }
 
 void zappy::Slavia::update(const f32 dt)
 {
-    constexpr f32 inclination_deg = 23.5f;
+    constexpr f32 inclination_deg = SLAVIA_INCLINATION_DEG;
     constexpr f32 inclination_rad = inclination_deg * DEG2RAD;
 
-    Vector3 pos = maths::orbit_position(0.5f, _zappy_radius, dt, 3.5f);
+    Vector3 pos = maths::orbit_position(SLAVIA_SPEED, _zappy_radius, dt, SLAVIA_OFFSET);
 
     const f32 y = pos.y * cosf(inclination_rad) - pos.z * sinf(inclination_rad);
     const f32 z = pos.y * sinf(inclination_rad) + pos.z * cosf(inclination_rad);
@@ -35,7 +43,7 @@ void zappy::Slavia::update(const f32 dt)
     pos.z = z;
 
     _model->setPosition(pos);
-    _rotation += 0.6f;
+    _rotation += SLAVIA_ROTATION_SPEED;
     zappy::BasePlanet::update(dt);
 }
 
